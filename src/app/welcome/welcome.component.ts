664b46372771808607ca7fef00a4cd3cb6dd7be1
Mihir Kurdekar <mihir.kurdekar@gmail.com>
2023-01-25 11:23:57 +0530

    test

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -5,8 +5,8 @@ import {Component, OnInit} from "@angular/core";
 import {UserType} from "../types/user.type";
 import {Router} from "@angular/router";
 import {UsersService} from "../services/users.service";
-import {map, takeUntil} from "rxjs/operators";
 import {Observable, Subject} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
     selector: "app-welcome",
@@ -16,6 +16,7 @@ import {Observable, Subject} from "rxjs";
 export class WelcomeComponent implements OnInit {
     user: UserType = {}; // type this variable using user.type.ts file
     private ngUnsubscribe = new Subject<void>();
+    users: UserType[];
 
     constructor(private router: Router, private usersService: UsersService) {
     }
@@ -23,17 +24,15 @@ export class WelcomeComponent implements OnInit {
     ngOnInit() {
         console.log(this.router.url);
         this.user.username = this.router.url.split('/')[2];
-        // this.getUsers();
+        this.getUsers();
     }
 
-    getUsers(): Observable<UserType[]> {
-        return this.usersService.getUsers()
-            .pipe(
-                map(value => {
-                    console.log(value['data']);
-                    return value['data'];
-                }),
-            );
+    getUsers(): void {
+        this.usersService.getUsers()
+            .pipe(map(value => {
+                console.log(value['data']);
+                this.users = value['data'];
+            })).subscribe();
     }
 
     ngOnDestroy() {
